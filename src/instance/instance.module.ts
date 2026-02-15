f9e822d74c781c5633dbeebea105ddb00a24592e
Beaux Walton <beaux.walton@gmail.com>
2026-02-15 13:33:59 -0800

    fix: add AccountModule import to InstanceModule (JwtService dependency)

--- src/instance/instance.module.ts
+++ src/instance/instance.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InstanceController } from './instance.controller';
 import { InstanceService } from './instance.service';
+import { AccountModule } from '../account/account.module';
 
 @Module({
+  imports: [AccountModule],
   controllers: [InstanceController],
   providers: [InstanceService],
   exports: [InstanceService],
