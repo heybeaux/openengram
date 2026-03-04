dafd986dc4df99e9ef632a8766dd42840a92f976
Beaux Walton <beaux.walton@gmail.com>
2026-03-04 15:27:25 -0800

    fix(consolidation): add ServicePrismaService with BYPASSRLS for background jobs (HEY-458)

--- src/prisma/service-prisma.module.ts
+++ src/prisma/service-prisma.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ServicePrismaService } from './service-prisma.service';
 
+/**
+ * Provides ServicePrismaService for background/system jobs.
+ * Import ONLY in modules that run background jobs.
+ * Do NOT import in AppModule.
+ */
 @Module({
   providers: [ServicePrismaService],
   exports: [ServicePrismaService],
